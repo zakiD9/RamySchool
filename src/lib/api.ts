5d94383cdc281a97694374c3740a455074a6bee4
zakiD9 <zakariadjerboa@gmail.com>
2025-11-13 13:40:11 +0100

    new link

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://studentsmanagement.runasp.net/api",
+  baseURL: "https://studentsmanagement.runasp.net/api",
   headers: {
     "Content-Type": "application/json",
   },
